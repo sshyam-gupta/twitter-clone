1f1db8ae8230b7d5e10ee3077d030a6add618bde
Shyam Gupta <shyamsundar.g@kiprosh.com>
2020-02-08 13:28:44 +0530

    Add prettier and lint

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,19 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <span>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        </span>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
     </div>
   );
-}
+};
 
 export default App;
